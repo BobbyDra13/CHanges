da3736596f41c1d9c344ab62f2b01a5818ef4d04
Paresh Kumar Barik <paresh.barik98@gmail.com>
2024-01-12 09:54:02 +0530

    capture % bug fixed

--- src/views/Customers/index.js
+++ src/views/Customers/index.js
@@ -342,7 +342,7 @@ const Customers = () => {
                               />
                               <button className="absolute hover:cursor-not-allowed w-full h-full flex justify-center place-items-center">
                                 <Typography sx={{ color: 'white' }} variant="subtitle2">
-                                  {Math.floor((item.capture_count || 0 / totalParts) * 100)} %
+                                  {Math.floor((item.capture_count / totalParts) * 100)} %
                                 </Typography>
                               </button>
                             </Box>
