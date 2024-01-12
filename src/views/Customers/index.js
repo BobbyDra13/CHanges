172497c8630a50fa99eea89d032010622b7fb858
Paresh Kumar Barik <paresh.barik98@gmail.com>
2024-01-12 09:59:14 +0530

    minor issue resolved

--- src/views/Customers/index.js
+++ src/views/Customers/index.js
@@ -337,7 +337,7 @@ const Customers = () => {
                                   }
                                 }}
                                 variant="determinate"
-                                value={Math.floor((item.capture_count || 0 / totalParts) * 100)}
+                                value={Math.floor((item.capture_count / totalParts) * 100)}
                                 color="secondary"
                               />
                               <button className="absolute hover:cursor-not-allowed w-full h-full flex justify-center place-items-center">
