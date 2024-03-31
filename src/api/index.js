6fae2f92026d9cfd7631a5fecb99746b5d4f3a0e
Paresh Kumar Barik <paresh.barik98@gmail.com>
2024-03-31 18:33:43 +0530

    insight page pop percentage and anomaly count kpi card api changed

--- src/api/index.js
+++ src/api/index.js
@@ -540,7 +540,7 @@ export const GetShelfData = async (data) => {
 
 export const GetPopPercentage = async (data) => {
   try {
-    const res = await axios.post('https://nifno3du90.execute-api.eu-west-2.amazonaws.com/test/web-app/dashboard/pop-score', data, {
+    const res = await axios.post('https://nifno3du90.execute-api.eu-west-2.amazonaws.com/test/web-app/dashboard/pop-score-2', data, {
       headers: {
         Accept: 'application/json',
         Authorization: await token()
