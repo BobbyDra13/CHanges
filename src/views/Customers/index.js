7c232a9b631349ad7ccc0184897bff3c6ab381e4
Rudra006 <rudramadhab006@gmail.com>
2024-04-03 11:09:08 +0530

    WhatsApp alert button activated

--- src/views/Customers/index.js
+++ src/views/Customers/index.js
@@ -401,9 +401,10 @@ const Customers = () => {
   // const navigate = useNavigate()
   useEffect(() => {
     async function sendAlertMsg() {
-      if (alertData.zone_id) {
-        console.log(alertData);
+      console.log(alertData);
+      if (alertData.user_id) {
         console.log('Number:', alertData.user_number);
+        const mssg = alertData.message === '' ? ' ' : alertData.message;
         const API_KEY =
           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NWY2MmE5Yzk4Nzk3MGFlZWM1ZTg0MCIsIm5hbWUiOiJOZW9QaHl0ZSIsImFwcE5hbWUiOiJBaVNlbnN5IiwiY2xpZW50SWQiOiI2NTVmNjJhOGM5ODc5NzBhZWVjNWU4M2IiLCJhY3RpdmVQbGFuIjoiQkFTSUNfTU9OVEhMWSIsImlhdCI6MTcwMDc0OTk5M30.8-SugzKOaRlF3BFhgTn944znZnsydeoUPudFEIZdNWs'; // Replace with your actual API key
         const API_URL = 'https://backend.aisensy.com/campaign/t1/api/v2';
@@ -411,20 +412,20 @@ const Customers = () => {
           return {
             apiKey: API_KEY,
             campaignName: 'disha_smart_alert_message_API_Campaign',
-            destination: '91' + '8085503475',
-            userName: 'Mayur Pawar',
+            destination: '91' + data.user_number,
+            userName: data.user_name,
             templateParams: ['$AgentName', '$BayId', '$ShelfId', '$AnomaliesTypes', '$BayId', '$ShelfId', '$CustomMessage'],
             tags: ['AgentName', 'BayId', 'ShelfId', 'AnomaliesTypes', 'BayId', 'ShelfId', 'CustomMessage'],
             attributes: {
-              AgentName: 'Mayur',
+              AgentName: data.user_name,
               BayId: data.zone_id,
               ShelfId: data.shelf_id,
               AnomaliesTypes: data.anomaly_type,
               //eslint-disable-next-line
               BayId: data.zone_id,
               //eslint-disable-next-line
-              ShelfId: 'Shelf- 4',
-              CustomMessage: 'Please the Anomalie'
+              ShelfId: data.shelf_id,
+              CustomMessage: mssg
             }
           };
         };
@@ -435,11 +436,11 @@ const Customers = () => {
 
         // const status = await SendAlert(alertData);
         // console.log(status);
-        // if (status.status === 200) {
-        setLoadsend(false);
-        setSnackbarOpen(true);
-        //  console.log('hello');
-        // }
+        if (status) {
+          setLoadsend(false);
+          setSnackbarOpen(true);
+          //  console.log('hello');
+        }
       }
     }
     sendAlertMsg();
