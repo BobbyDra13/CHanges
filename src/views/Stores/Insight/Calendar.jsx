6e3aa3402915edcd0f393be3f5d1d6cdc2d0a5b3
Krishna-windows <krshnabiswal619@gmail.com>
2024-04-23 16:34:51 +0530

    Refactor logging statements and update date handling in Insights and Calendar components

--- src/views/Stores/Insight/Calendar.jsx
+++ src/views/Stores/Insight/Calendar.jsx
@@ -85,8 +85,9 @@ function DatePickerComp({ SetSelectedDate }) {
 
   const handlechange = (date) => {
     setCalender(date);
-    // console.log(date.getDate())
+    console.log('def cat', date.getDate());
   };
+
   function padTo2Digits(num) {
     return num.toString().padStart(2, '0');
   }
