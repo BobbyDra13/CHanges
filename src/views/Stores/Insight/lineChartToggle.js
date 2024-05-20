e7b6b229069621bf1f4c69e1b34fe3f018372f1a
Harsh Lohana <harshylohana15@gmail.com>
2024-05-20 14:47:54 +0530

    responsive

--- src/views/Stores/Insight/lineChartToggle.js
+++ src/views/Stores/Insight/lineChartToggle.js
@@ -1,4 +1,4 @@
-import { Grid, Paper } from '@mui/material';
+import { Grid, Paper, useMediaQuery, useTheme } from '@mui/material';
 import { useEffect, useState } from 'react';
 // import Chart from 'react-apexcharts';
 import StoreView from './storeView';
@@ -11,8 +11,8 @@ const LineChartToggle = ({ storeId, date }) => {
     setActiveButton(button);
   };
 
-  // const isSmallScreen = !useMediaQuery(theme.breakpoints.up('sm'));
-  // const isMediumScreen = !useMediaQuery(theme.breakpoints.up('md'));
+  const theme = useTheme();
+  const isSmallScreen = !useMediaQuery(theme.breakpoints.up('sm'));
 
   // const data = {
   //   VM: {
@@ -137,7 +137,9 @@ const LineChartToggle = ({ storeId, date }) => {
           borderRadius: '15px'
         }}
       >
-        <div style={{ padding: '2px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '1%' }}>
+        <div
+          style={{ padding: '2px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingBottom: '1%', width: 'full' }}
+        >
           <button
             onClick={() => handleButtonClick('Trends View')}
             style={{
@@ -146,7 +148,7 @@ const LineChartToggle = ({ storeId, date }) => {
               padding: '4px',
               borderRadius: '17px 0 0 17px',
               marginRight: '3px',
-              width: '15%',
+              width: isSmallScreen ? '30%' : '15%',
               fontSize: '1rem'
             }}
           >
@@ -159,7 +161,7 @@ const LineChartToggle = ({ storeId, date }) => {
               color: '#fff',
               padding: '4px',
               marginRight: '3px',
-              width: '15%',
+              width: isSmallScreen ? '30%' : '15%',
               fontSize: '1rem',
               cursor: 'not-allowed'
             }}
@@ -173,7 +175,7 @@ const LineChartToggle = ({ storeId, date }) => {
               color: '#fff',
               padding: '4px',
               borderRadius: '0 17px 17px 0',
-              width: '15%',
+              width: isSmallScreen ? '30%' : '15%',
               fontSize: '1rem'
             }}
           >
@@ -183,7 +185,7 @@ const LineChartToggle = ({ storeId, date }) => {
         {activeButton == 'Trends View' ? (
           <div style={{ width: '100%', overflow: 'auto', height: '500px' }}>
             {/* <Chart options={chartOptions} series={series} type="line" height={500} width={isSmallScreen || isMediumScreen ? 720 : '100%'} /> */}
-            <TrendsChart storeId={storeId} date={date} />
+            <TrendsChart storeId={storeId} date={date} isSmallScreen={isSmallScreen} />
             {/* <Grid
               container
               spacing={1}
