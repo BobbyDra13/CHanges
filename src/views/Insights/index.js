80f556e94561dee65662aa18b58015e87bfa7114
pratyush <pratyush2582002@gmail.com>
2024-07-05 21:09:51 +0530

    nothing to update

--- src/views/Insights/index.js
+++ src/views/Insights/index.js
@@ -1075,7 +1075,7 @@ const Insights = () => {
                   <Grid container spacing={gridSpacing}>
                     {capProgress ? (
                       // capProgress.map((item) => (
-                      capProgress.map((item, key) => {
+                     capProgress.length > 0 &&  capProgress.map((item, key) => {
                         return (
                           <Grid key={key} item xs={12}>
                             <Grid container justifyContent={'space-between'} alignItems="center" spacing={1}>
